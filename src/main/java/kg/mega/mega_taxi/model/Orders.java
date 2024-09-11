c844490e9bb4a29d77cc7cd1af8dbe47d03ce97a
Erbol <esembayev.e@gmail.com>
2024-09-11 19:26:47 +0600

    orders

--- src/main/java/kg/mega/mega_taxi/model/Orders.java
+++ src/main/java/kg/mega/mega_taxi/model/Orders.java
@@ -1,4 +1,34 @@
 package kg.mega.mega_taxi.model;
 
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
 public class Orders {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "source_point")
+    private String sourcePoint;
+
+    @Column(name = "destination_point")
+    private String destinationPoint;
+
+    @OneToOne
+    @JoinColumn(name = "client")
+    private Users client;
+
+    @OneToOne
+    @JoinColumn(name = "driver")
+    private Users driver;
+
+    private double price;
+
+    @OneToOne
+    @JoinColumn(name = "status_order")
+    private OrderStatus statusOrder;
+
 }
