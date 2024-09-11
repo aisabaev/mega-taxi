1bd00cf6fc90da534f77597423cf8679218ffa09
ayazFromCentralAsia <wiliamraartur@gmail.com>
2024-09-11 18:48:27 +0600

    Закончил работу с entity

--- src/main/java/kg/mega/mega_taxi/model/OrderStatus.java
+++ src/main/java/kg/mega/mega_taxi/model/OrderStatus.java
@@ -1,4 +1,15 @@
 package kg.mega.mega_taxi.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "order_status")
 public class OrderStatus {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "status_name")
+    private String status_name;
 }
