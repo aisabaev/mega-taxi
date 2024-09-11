47921bd153c237debd049bdeb2df7338d9636fe6
Erbol <esembayev.e@gmail.com>
2024-09-11 19:33:52 +0600

    added repository for orders & orders history

---
+++ src/main/java/kg/mega/mega_taxi/repository/OrdersRepository.java
@@ -0,0 +1,9 @@
+package kg.mega.mega_taxi.repository;
+
+import kg.mega.mega_taxi.model.Orders;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrdersRepository extends JpaRepository<Orders, Long> {
+}
